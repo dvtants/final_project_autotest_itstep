d8f82fa5f7683d258567212b9f4fa8370d2baeb3
dvtants <dv.tants@gmail.com>
2023-10-26 20:58:08 +0300

    fixed start window_size of firefox

--- test_1.py
+++ test_1.py
@@ -1,19 +1,15 @@
+class TestSuite:
+    def test_1(self, browser):
+        browser.get('https://casenik.com.ua/')
 
-
-
-
-
-
-
-
-
-
+    def test_2(self, browser):
+        browser.get('https://casenik.com.ua/')
 
 
 
 
 # В терміналі PyCharm:
-# pytest -s -v test_1.py
+# pytest -s -v test_1.py {Якщо файл один - то ми його не прописуємо!!!}
 # pytest -s -v -m "smoke" test_1.py - виконується один тест тільки смоук, а саме: @pytest.mark.smoke
 # pytest -s -v -m "regression" test_1.py - виконується два тести де є рігрешн, а саме: @pytest.mark.regression
 # pytest -s -v -m " not regression" test_1.py - виконується все окрім рігрешн.
@@ -21,10 +17,12 @@
 # pytest -s -v -m "regression or smoke" test_1.py - все виконується.
 # pytest -s -v -m "regression and smoke" test_1.py - нічого не виконується.
 # pytest -s -v -m "regression and chrome_117" test_1.py - проходить тест де є і regression і chrome_117, а саме: @pytest.mark.chrome_117 і @pytest.mark.regression
-# pytest -s -v --browser_mode="gui" test_1.py
+# pytest -s -v --browser_mode="gui" test_1.py {Якщо файл один - то ми його не прописуємо!!!}
 # pytest -s -v --browser_mode="gui" --browser_name="firefox" test_1.py
 # дивись файл pytest.ini - зникає warnings - попередження.
 # дивись файл conftest.py - назва цього файла саме така і не повинна змінюватися.
-
-
-
+# {Якщо файл один - то ми його не прописуємо!!!}
+# pytest -s -v {chrome, headless}
+# pytest -s -v --browser_mode="gui" {chrome, gui}
+# pytest -s -v --browser_name="firefox" {firefox, headless}
+# pytest -s -v --browser_mode="gui" --browser_name="firefox"
