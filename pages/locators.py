b052da0722cc140281cfea2cad343f04d9a4fa54
dvtants <dv.tants@gmail.com>
2023-11-02 20:38:50 +0200

    edit base_page and locators

--- pages/locators.py
+++ pages/locators.py
@@ -17,7 +17,12 @@ class BasePageLocators:
     BUTTON_SUBMIT = (By.XPATH, "//button[@class='header_search_button trans_300']")
     WISH_SHOW = (By.XPATH, "//img[@src='images/heart.png']")
     CART_SHOW = (By.XPATH, "//img[@src='images/cart.png']")
-
-
-
-
+    SHOW_NEW = (By.XPATH, "//span[text()='Новинки']")
+    SHOW_SALE = (By.XPATH, "//span[text()='Скидки']")
+    SHOW_HIT = (By.XPATH, "//span[text()='Хиты']")
+    SAMSUNG_CAT = (By.XPATH, "//div[text()='Samsung']")
+    SAMSUNG_J701 = (By.XPATH,
+                    "//a[text()='Samsung J701']")  # В консолі Chrome робимо заморозку: setTimeout(function(){debugger;}, 5000) - на 5 сек.
+    SUBSCRIBE = (By.XPATH, "//button[text()='Подписаться!']")  # //button[@class="newsletter_button"]
+    INPUT_SUBSCRIBE = (By.XPATH, "//input[@name='submail']")
+    LOGO_FOOTER = (By.XPATH, "//img[@src='images/logo-footer.png']")
